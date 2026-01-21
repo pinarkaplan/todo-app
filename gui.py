a72c4b4b069725c738dbac798c7383ef3c259ed4
pinarkaplan <pinarkpln.3105@gmail.com>
2026-01-21 15:29:20 -0600

    Implement the Complete and Exit buttons

--- gui.py
+++ gui.py
@@ -8,9 +8,14 @@ add_button=sg.Button("Add")
 list_box=sg.Listbox(values=functions.get_todos(),key="box_todos",
                     enable_events=True,size=[45,10])
 edit_button=sg.Button("Edit")
+complete_button=sg.Button("Complete")
+exit_button=sg.Button("Exit")
 
 window=sg.Window("My To-Do App",
-                 layout=[[label],[input_box,add_button],[list_box,edit_button]],
+                 layout=[[label],
+                         [input_box,add_button],
+                         [list_box,edit_button , complete_button],
+                         [exit_button]],
                  font=("Helvetica",20))
 
 while True:
@@ -20,7 +25,7 @@ while True:
     match event:
         case "Add":
             todos=functions.get_todos()
-            new_todo=values["input_to-do"]+"\n"
+            new_todo=values["input_to-do"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
             window["box_todos"].update(values=todos)
@@ -32,13 +37,19 @@ while True:
             todos[index]=new_todo
             functions.write_todos(todos)
             window["box_todos"].update(values=todos)
+        case "Complete":
+            todo_to_complete=values["box_todos"][0]
+            todos=functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["box_todos"].update(values=todos)
+            window["input_to-do"].update(value="")
+        case "Exit":
+            break
         case "box_todos":
             window["input_to-do"].update(value=values["box_todos"][0])
         case sg.WIN_CLOSED:
             break
-
-
-
 
 
 window.close()
